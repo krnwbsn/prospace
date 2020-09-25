5310762c554f17f9229c8815b6f3b523d3130245
krnwbsn <krinawibisana@gmail.com>
2020-09-25 15:46:12 +0700

    company: add get company data by id

--- server/routes/company.js
+++ server/routes/company.js
@@ -15,4 +15,11 @@ router.get('/', function (req, res) {
   res.status(200).json(company);
 });
 
+router.get('/:id', function (req, res) {
+  const { id } = req.params;
+  const data = company.filter((item) => item.id === parseInt(id));
+
+  res.status(200).json(data);
+});
+
 module.exports = router;
