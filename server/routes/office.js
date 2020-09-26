4fc9d59f1755f2dadb0bc8d55b9ee7eb2c3b757b
krnwbsn <krinawibisana@gmail.com>
2020-09-26 07:46:15 +0700

    office: update post and get by id router

--- server/routes/office.js
+++ server/routes/office.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const moment = require('moment');
-const bodyParser = require('body-parser');
 const office = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../data/office.json'), 'utf8')
 );
@@ -12,14 +10,17 @@ const writeData = (data) =>
     path.resolve(__dirname, '../data/office.json'),
     JSON.stringify(data, null, 2)
   );
+const moment = require('moment');
+const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get('/:companyName', function (req, res) {
-  const { companyName } = req.params;
+router.get('/:id', function (req, res) {
+  const { id } = req.params;
+  console.log(id)
   const data = office.filter(
-    (item) => item.companyName.toLowerCase() === companyName.toLowerCase()
+    (item) => item.companyId === parseInt(id)
   );
 
   res.status(200).json(data);
@@ -27,7 +28,7 @@ router.get('/:companyName', function (req, res) {
 
 router.post('/', function (req, res) {
   const id = moment().unix() + 12;
-  const { companyName, officeName, lat, log, startDate } = req.body;
+  const { companyName, officeName, lat, log, startDate, companyId } = req.body;
   const cap = (text) => {
     return text.replace(/(^\w{1})|(\s{1}\w{1})/g, (match) =>
       match.toUpperCase()
@@ -36,6 +37,7 @@ router.post('/', function (req, res) {
 
   const item = {
     officeId: id,
+    companyId: companyId,
     companyName: cap(companyName),
     officeName: cap(officeName),
     location: { lat: parseFloat(lat), log: parseFloat(log) },
@@ -55,8 +57,7 @@ router.delete('/:officeId', function (req, res) {
   const data = office.filter((item) => item.officeId !== parseInt(officeId));
   writeData(data);
 
-  console.log(`data ${officeId} removed successfully`);
-  res.status(204).send({message: `data removed successfully`});
+  res.status(204);
 });
 
 module.exports = router;
